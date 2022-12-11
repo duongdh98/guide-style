9fda5382ffeb790369c4eba12f97b23334ba9ab7
duongdh98 <65645301+duongdh98@users.noreply.github.com>
2022-12-11 08:51:17 -0800

    c next exam opp

--- C_Next/src/oop.cpp
+++ C_Next/src/oop.cpp
@@ -118,3 +118,12 @@ Dog::~Dog(){};
 string Dog::getSound() {
      return "Woof! Woof!";
 }
+
+template <class T> void bubbleSort(T a[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+        for (int j = n - 1; i < j; j--)
+            if (a[j] < a[j - 1])
+                swap(a[j], a[j - 1]);
+}
+  
